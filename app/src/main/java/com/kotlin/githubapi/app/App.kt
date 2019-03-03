d73d6a0645fb0ea8e8f5b9961f4d45a16b55ed93
Alexander <alex93anokhin@gmail.com>
2019-03-03 22:20:04 +0300

    Crashes fixed.

--- app/src/main/java/com/kotlin/githubapi/app/App.kt
+++ app/src/main/java/com/kotlin/githubapi/app/App.kt
@@ -1,7 +1,5 @@
 package com.kotlin.githubapi.app
 
-import android.content.Context
-import androidx.multidex.MultiDex
 import com.kotlin.githubapi.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
@@ -11,10 +9,5 @@ class App : DaggerApplication() {
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.builder().application(this).build()
-    }
-
-    override fun attachBaseContext(base: Context?) {
-        super.attachBaseContext(base)
-        MultiDex.install(this)
     }
 }
