10a36cbc6afe7be4f730f385cfd204e4aee47bcd
Alexander <alex93anokhin@gmail.com>
2019-03-05 22:31:52 +0300

    Splash screen added.

--- app/src/main/java/com/kotlin/githubapi/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/kotlin/githubapi/ui/splash/SplashActivity.kt
@@ -1,9 +1,18 @@
 package com.kotlin.githubapi.ui.splash
 
+import android.content.Intent
+import android.os.Bundle
 import com.kotlin.githubapi.R
 import com.kotlin.githubapi.ui.base.BaseActivity
+import com.kotlin.githubapi.ui.main.MainActivity
 
 class SplashActivity : BaseActivity() {
 
-    override fun getLayoutResource(): Int = R.layout.main_activity
+    override fun getLayoutResource(): Int = R.layout.splash_activity
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+        finishAffinity()
+    }
 }
